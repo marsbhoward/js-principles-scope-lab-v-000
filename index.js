c9ecffe68c71749f2e416dc2efde48191ec239e7
Mars Howard <marsbhoward@gmail.com>
2019-03-12 20:24:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,23 @@
 var customerName = 'bob'
 
 function upperCaseCustomerName() {
-  customerName  =customerName.toUpperCase();
+    return customerName  =customerName.toUpperCase();
+}
 
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+    return bestCustomer
+}
+
+function overwriteBestCustomer(){
+   bestCustomer = "maybe bob"
+}
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "not bob"
+}
+
+function attemptTwoFavoriteCustomers(){
+    let FavoriteCustomer = "bob"
+    let FavoriteCustomer = "mars"
 }
