ca543e95588c876c7efbee519e5da51df73320de
Mars Howard <marsbhoward@gmail.com>
2019-03-12 20:40:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName() {
+    return customerName  =customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+    return bestCustomer
+}
+
+function overwriteBestCustomer(){
+   bestCustomer = "maybe bob"
+}
+
+const leastFavoriteCustomer = 'mars'
+function changeLeastFavoriteCustomer () {
+   leastFavoriteCustomer = 43;
+}
+
+function attemptTwoFavoriteCustomers () {
+  let favoriteCustomer = 'bob';
+  let favoriteCustomer = 'not bob';
+}
